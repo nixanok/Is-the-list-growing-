4cff52d859164c7c2f71fe960abda09fe9dff4b8
nixanok <mrfreemen555@gmail.com>
2023-03-15 11:55:33 +0300

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String[] stringNumbers = s.split(" ");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (String stringNumber : stringNumbers) {
+            numbers.add(Integer.parseInt(stringNumber));
+        }
 
+        for(int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) >= numbers.get(i + 1)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
     }
 }
